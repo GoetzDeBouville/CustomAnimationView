5a66c206b7a821ad3a8ed0f35f5b7307707611fd
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-05-18 14:30:01 +0300

    draw heart

--- app/src/main/java/com/hellcorp/customanimationview/hearts/HeartsView.kt
+++ app/src/main/java/com/hellcorp/customanimationview/hearts/HeartsView.kt
@@ -1,10 +1,20 @@
 package com.hellcorp.customanimationview.hearts
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 import androidx.annotation.AttrRes
 import androidx.annotation.StyleRes
+import androidx.core.content.ContextCompat
+import com.hellcorp.customanimationview.R
+import kotlin.math.min
 
 class HeartsView @JvmOverloads constructor(
     context: Context,
@@ -17,9 +27,39 @@ class HeartsView @JvmOverloads constructor(
     deffstyleAttr,
     deffstyleRes
 ) {
-    private fun Float.dpToFloat(): Float {
-        val scale = resources.displayMetrics.density
-        return (this * scale)
+    private val minViewSize = resources.getDimensionPixelSize(
+        R.dimen.hearts_view_size
+    )
+    private val paint = Paint().apply {
+        color = ContextCompat.getColor(context, R.color.heart_color1)
+        style = Paint.Style.FILL
+    }
+    private val path = Path()
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val size = min(
+            MeasureSpec.getSize(widthMeasureSpec).coerceAtMost(minViewSize),
+            MeasureSpec.getSize(heightMeasureSpec).coerceAtMost(minViewSize)
+        )
+        setMeasuredDimension(size, size)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        path.reset()
+        path.moveTo(width / 2f, height / 4f)
+        path.cubicTo(
+            width / 5f, 0f,
+            0f, height / 2.5f,
+            width / 2f, height * 3 / 4f
+        )
+        path.cubicTo(
+            width.toFloat(), height / 2.5f,
+            width * 4 / 5f, 0f,
+            width / 2f, height / 4f
+        )
+        path.close()
+        canvas.drawPath(path, paint)
     }
 }
-
