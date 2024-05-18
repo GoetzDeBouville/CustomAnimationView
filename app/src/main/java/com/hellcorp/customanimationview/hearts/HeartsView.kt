af200825c2f00f12b496507b9b0dbbaa6e7c86b5
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-05-18 18:21:12 +0300

    add animation for appearance and pulsing

--- app/src/main/java/com/hellcorp/customanimationview/hearts/HeartsView.kt
+++ app/src/main/java/com/hellcorp/customanimationview/hearts/HeartsView.kt
@@ -1,11 +1,11 @@
 package com.hellcorp.customanimationview.hearts
 
+import android.animation.Animator
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
@@ -27,6 +27,10 @@ class HeartsView @JvmOverloads constructor(
     deffstyleAttr,
     deffstyleRes
 ) {
+    init {
+        animateAppearance()
+    }
+
     private val minViewSize = resources.getDimensionPixelSize(
         R.dimen.hearts_view_size
     )
@@ -61,5 +65,52 @@ class HeartsView @JvmOverloads constructor(
         )
         path.close()
         canvas.drawPath(path, paint)
+    }
+
+    private fun startPulsingAnimation() {
+        val scaleX = ObjectAnimator.ofFloat(this, PROPERTY_SCALE_X, 1f, 1.2f, 1f)
+        val scaleY = ObjectAnimator.ofFloat(this, PROPERTY_SCALE_Y, 1f, 1.2f, 1f)
+        val animatorSet = AnimatorSet().apply {
+            playTogether(scaleX, scaleY)
+            duration = 1000
+            reverse()
+        }
+        val valueAnimator = ValueAnimator.ofFloat(0f, 1f)
+        valueAnimator.repeatCount = ValueAnimator.INFINITE
+        valueAnimator.addUpdateListener {
+            animatorSet.reverse()
+        }
+        valueAnimator.start()
+    }
+
+    private fun transitionAnimation() {
+        TODO("build transition animation")
+    }
+
+    private fun animateAppearance() {
+        val scaleX = ObjectAnimator.ofFloat(this, "scaleX", 0f, 1f)
+        val scaleY = ObjectAnimator.ofFloat(this, "scaleY", 0f, 1f)
+
+        val animatorSet = AnimatorSet().apply {
+            playTogether(scaleX, scaleY)
+            duration = 700
+
+            addListener(object : Animator.AnimatorListener {
+                override fun onAnimationStart(animation: Animator) = Unit
+                override fun onAnimationEnd(animation: Animator) {
+                    startPulsingAnimation()
+                }
+
+                override fun onAnimationCancel(animation: Animator) = Unit
+                override fun onAnimationRepeat(animation: Animator) = Unit
+            })
+        }
+        animatorSet.start()
+    }
+
+    companion object {
+        const val PROPERTY_SCALE_X = "scaleX"
+        const val PROPERTY_SCALE_Y = "scaleY"
+        const val PROPERTY_ALPHA = "alpha"
     }
 }
